c3b087614efe79c07b43c633ce5c6e2d5696d226
sebastian garay <sebastiansalamanca844@gmail.com>
2025-11-25 21:45:46 -0500

    index.js define get/,get,/health

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+//DEFINIR RUTAS
+app.get('/health' , (req,res) => {
+    res.send('<h1>health</h1>');
+
+});
+
+//LANZANDO EL SERVIDOR WEB USANDO EXPRESS
 app.listen(PORT, () => {
-    console.log(`server running on http://localhost: ${PORT} :( )`);
+    console.log(`server running on http://localhost:${PORT}/health :()`);
 });
