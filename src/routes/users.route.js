99598d17f1962447c43e530e940b0ba7693161e9
Ramiro Diaz <jramiroda10@gmail.com>
2025-12-01 20:50:18 -0500

    [+]user.controller.js se creo un controlador para tener mas limpio la ruta

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,26 +1,11 @@
 //IMPORTANDO LA DEPENDECIA EXPRESS USANDO commonJs
-import express from  'express';
-import userModel from '../models/user.model.js';
-const router = express.Router();
+import { Router} from  'express';
+
+import { createUser } from '../controllers/user.controller.js';
+const router = Router();
 //DEFINICION DE LAS RUTAS (ENDPOINTS)
 
-router.post('/', async (req, res) => {
-    const data  = req.body;                     //extraer el cuerpo de la peticion
-
-    console.log( data );                        // imprimirr en la consola el cuerpo de la  peticion
-    
-    //registrar los datos usandon uselModel
-    const dataRegistered = await userModel.create( data );             //registrar los datos en la base de datos
-
-    // responder al usuario
-
-    res.json({ msg: 'create users',
-        // data: data;     //forma tradicional 
-        dataRegistered     // ECMAScript 2015
-     })
-})
-
-
+router.post('/',  createUser );
 
 
 // Definicion de las rutas (EndPoints)
