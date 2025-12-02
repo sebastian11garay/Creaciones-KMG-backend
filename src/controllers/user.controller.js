7ced84f29a6e6afa0158a68f443d74e6e2b6ebce
Ramiro Diaz <jramiroda10@gmail.com>
2025-12-01 20:51:33 -0500

    [+]user.controller.js se creo un controlador para tener mas limpio la ruta

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,23 @@
+import userModel from "../models/user.model.js";
+
+const  createUser = async (req, res) => {
+    const data  = req.body;                     //extraer el cuerpo de la peticion
+
+    console.log( data );                        // imprimirr en la consola el cuerpo de la  peticion
+    
+    //registrar los datos usandon uselModel
+    const dataRegistered = await userModel.create( data );             //registrar los datos en la base de datos
+
+    // responder al usuario
+
+    res.json({ 
+        msg: 'create users',
+        // data: data;     //forma tradicional 
+        dataRegistered     // ECMAScript 2015
+     });
+}
+
+export { 
+    createUser
+
+} 
